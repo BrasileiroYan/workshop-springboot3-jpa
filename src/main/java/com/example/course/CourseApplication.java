c142fbdec41879ad94fc2d7f4db6b65f7ec4c662
Yan Pedro <yanpedrobrasileiro@gmail.com>
2025-04-23 16:59:08 -0300

    User entity and Resource

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -1,9 +1,11 @@
 package com.example.course;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CourseApplication {
 
 	public static void main(String[] args) {
