ce105e8fed1c6ad2b7f90f5e6628a99707c9e608
Yan Pedro <yanpedrobrasileiro@gmail.com>
2025-04-23 18:05:20 -0300

    H2 database, test profile

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -3,9 +3,8 @@ package com.example.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
